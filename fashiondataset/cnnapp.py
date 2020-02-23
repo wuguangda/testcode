7920a97f6de1f3b2056bcac705150c3451f4f45a
wuguangda <653322694@qq.com>
2020-02-23 17:00:08 +0800

    gdwu  修改网络

--- fashiondataset/cnnapp.py
+++ fashiondataset/cnnapp.py
@@ -6,9 +6,11 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import tensorflow as tf
 from tensorflow import keras
 
+
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 fashion_mnist = keras.datasets.fashion_mnist
 
@@ -21,5 +23,47 @@ test_images = test_images / 255.0
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
+#标签个数
+num_classes = 10
 
+model = keras.models.Sequential()
+model.add(keras.layers.Conv1D(32, 3, padding='same', input_shape=(28, 28)))
+model.add(keras.layers.Activation('relu'))
+model.add(keras.layers.Conv1D(32, 3))
+model.add(keras.layers.Activation('relu'))
+model.add(keras.layers.MaxPooling1D(pool_size=2))
+model.add(keras.layers.Dropout(0.25))
+
+model.add(keras.layers.Conv1D(64, 3, padding='same'))
+model.add(keras.layers.Activation('relu'))
+model.add(keras.layers.Conv1D(64, 3))
+model.add(keras.layers.Activation('relu'))
+model.add(keras.layers.MaxPooling1D(pool_size=2))
+model.add(keras.layers.Dropout(0.25))
+
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(64, activation='relu'))
+#model.add(keras.layers.Activation('relu'))
+model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dense(num_classes, activation = 'softmax'))
+#model.add(keras.layers.Activation('softmax'))
+
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=20)
+
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+
+print('\nTest accuracy:', test_acc)
+
+
+predictions = model.predict(test_images)
+
+print(predictions[0])
+
+print("sss ",np.argmax(predictions[0]))
 

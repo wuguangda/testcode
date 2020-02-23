37c50e4e23e0ebe79d2087d1513c63a8038caca3
wuguangda <653322694@qq.com>
2020-02-23 15:12:03 +0800

    [gdwu] 修改问题

---
+++ fashiondataset/cnnapp.py
@@ -0,0 +1,25 @@
+#!/bin/python
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+
